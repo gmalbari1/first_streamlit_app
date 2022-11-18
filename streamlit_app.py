8abd0dbf67ba7f0ec52659322be12405242f4625
Gaurav Malbari <118509225+gmalbari1@users.noreply.github.com>
2022-11-18 13:23:36 +0530

    Update streamlit_app.py
    Added new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.text('🥣 Omega 3 & blueberry oat meal')
 streamlit.text('🥗 Kale Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avaocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
